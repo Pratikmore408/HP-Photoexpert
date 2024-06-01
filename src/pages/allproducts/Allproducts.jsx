a51cacef0f5f759c2e640eae949ccc100acdf773
Pratik More <pratikmore408@gmail.com>
2024-06-01 20:35:12 +0530

    reload for product info fixed

--- src/pages/allproducts/Allproducts.jsx
+++ src/pages/allproducts/Allproducts.jsx
@@ -3,6 +3,7 @@ import React, { useContext, useEffect } from "react";
 import myContext from "../../context/data/myContext";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart } from "../../redux/cartSlice";
+import { Link } from "react-router-dom";
 
 function Allproducts() {
   // Accessing context and required functions
@@ -48,54 +49,56 @@ function Allproducts() {
             const { title, price, imageUrl, id } = item;
             return (
               <div
-                onClick={() => (window.location.href = `/productinfo/${id}`)}
-                key={index}
+                // onClick={() => (window.location.href = `/productinfo/${id}`)}
+                // key={index}
                 className="p-4 md:w-1/4  drop-shadow-lg "
               >
-                <div
-                  className="h-full border-2 hover:shadow-gray-100 hover:shadow-2xl transition-shadow duration-300 ease-in-out    border-gray-200 border-opacity-60 rounded-2xl overflow-hidden"
-                  style={{
-                    backgroundColor: mode === "dark" ? "rgb(46 49 55)" : "",
-                    color: mode === "dark" ? "white" : "",
-                  }}
-                >
-                  <div className="flex justify-center cursor-pointer">
-                    <img
-                      className=" rounded-2xl w-full h-80 p-2 hover:scale-110 transition-scale-110  duration-300 ease-in-out"
-                      src={imageUrl}
-                      alt="blog"
-                    />
-                  </div>
-                  <div className="p-5 border-t-2">
-                    <h2
-                      className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1"
-                      style={{ color: mode === "dark" ? "white" : "" }}
-                    >
-                      HP-Photoexpert
-                    </h2>
-                    <h1
-                      className="title-font text-lg font-medium text-gray-900 mb-3"
-                      style={{ color: mode === "dark" ? "white" : "" }}
-                    >
-                      {title}
-                    </h1>
-                    <p
-                      className="leading-relaxed mb-3"
-                      style={{ color: mode === "dark" ? "white" : "" }}
-                    >
-                      ₹{price}
-                    </p>
-                    <div className=" flex justify-center">
-                      <button
-                        type="button"
-                        onClick={() => addCart(item)}
-                        className="focus:outline-none text-white bg-pink-600 hover:bg-pink-700 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm w-full  py-2"
+                <Link to={`/productinfo/${id}`}>
+                  <div
+                    className="h-full border-2 hover:shadow-gray-100 hover:shadow-2xl transition-shadow duration-300 ease-in-out    border-gray-200 border-opacity-60 rounded-2xl overflow-hidden"
+                    style={{
+                      backgroundColor: mode === "dark" ? "rgb(46 49 55)" : "",
+                      color: mode === "dark" ? "white" : "",
+                    }}
+                  >
+                    <div className="flex justify-center cursor-pointer">
+                      <img
+                        className=" rounded-2xl w-full h-80 p-2 hover:scale-110 transition-scale-110  duration-300 ease-in-out"
+                        src={imageUrl}
+                        alt="blog"
+                      />
+                    </div>
+                    <div className="p-5 border-t-2">
+                      <h2
+                        className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1"
+                        style={{ color: mode === "dark" ? "white" : "" }}
                       >
-                        Add To Cart
-                      </button>
+                        HP-Photoexpert
+                      </h2>
+                      <h1
+                        className="title-font text-lg font-medium text-gray-900 mb-3"
+                        style={{ color: mode === "dark" ? "white" : "" }}
+                      >
+                        {title}
+                      </h1>
+                      <p
+                        className="leading-relaxed mb-3"
+                        style={{ color: mode === "dark" ? "white" : "" }}
+                      >
+                        ₹{price}
+                      </p>
+                      <div className=" flex justify-center">
+                        <button
+                          type="button"
+                          onClick={() => addCart(item)}
+                          className="focus:outline-none text-white bg-pink-600 hover:bg-pink-700 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm w-full  py-2"
+                        >
+                          Add To Cart
+                        </button>
+                      </div>
                     </div>
                   </div>
-                </div>
+                </Link>
               </div>
             );
           })}
